49707cfafa1b12666e9d5b08c29dc6a4c0859678
Daniel Palmer <daniel@0x0f.com>
2019-03-21 18:26:27 +0900

    MSC313e: Do io remap if DEBUG_LL is enabled so we know where the uart is for earlyprintk

--- arch/arm/mach-mstar/mstar.c
+++ arch/arm/mach-mstar/mstar.c
@@ -22,11 +22,33 @@
 #include <linux/clk-provider.h>
 #include <linux/clocksource.h>
 
+#ifdef CONFIG_DEBUG_LL
+#define MS_IO_OFFSET			0xDE000000
+#define IO_PHYS                 0x1F000000
+#define IO_VIRT                 (IO_PHYS+IO_OFFSET)
+#define IO_OFFSET               (MS_IO_OFFSET)
+#define IO_SIZE                 0x00400000
+
+static struct map_desc mstar_io_desc[] __initdata =
+{
+		{IO_VIRT,   __phys_to_pfn(IO_PHYS),     IO_SIZE,        MT_DEVICE},
+};
+
+static void __init mstar_map_io(void)
+{
+	iotable_init(mstar_io_desc, ARRAY_SIZE(mstar_io_desc));
+}
+#endif
+
 static const char * const mstar_board_dt_compat[] = {
 	"mstar,msc313e",
 	NULL,
 };
 
+
 DT_MACHINE_START(MSTAR_DT, "MStar Cortex-A7 (Device Tree)")
 	.dt_compat	= mstar_board_dt_compat,
+#ifdef CONFIG_DEBUG_LL
+	.map_io = mstar_map_io,
+#endif
 MACHINE_END
