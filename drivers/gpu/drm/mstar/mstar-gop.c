9c4b738660967f0670e6ebc1ee2f1480eed0a547
Daniel Palmer <daniel@0x0f.com>
2019-12-08 14:28:37 +0900

    gop

--- drivers/gpu/drm/mstar/mstar-gop.c
+++ drivers/gpu/drm/mstar/mstar-gop.c
@@ -4,10 +4,43 @@
 #include <linux/platform_device.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/regmap.h>
 #include "mstar-gop.h"
+
+struct mstar_gop {
+	struct device *dev;
+	struct regmap *regmap;
+};
+
+static const struct regmap_config mstar_gop_regmap_config = {
+		.name = "mstar-gop",
+		.reg_bits = 16,
+		.val_bits = 16,
+		.reg_stride = 4,
+};
 
 static int mstar_gop_probe(struct platform_device *pdev)
 {
+	struct mstar_gop *gop;
+	struct resource *mem;
+	__iomem void *base;
+
+	gop = devm_kzalloc(&pdev->dev, sizeof(*gop), GFP_KERNEL);
+	if(!gop){
+		return -ENOMEM;
+	}
+
+	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	base = devm_ioremap_resource(&pdev->dev, mem);
+	if (IS_ERR(base))
+		return PTR_ERR(base);
+
+	gop->regmap = devm_regmap_init_mmio(&pdev->dev, base,
+			&mstar_gop_regmap_config);
+	if(IS_ERR(gop->regmap)){
+		return PTR_ERR(gop->regmap);
+	}
+
 	return 0;
 }
 
