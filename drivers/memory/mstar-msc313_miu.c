e5771bc252b62ce2f3d4bdbf51e8f24a5f32dd09
Daniel Palmer <daniel@0x0f.com>
2019-12-31 01:42:07 +0900

    miu: let the kernel dangerously adjust dram timing wooo

--- drivers/memory/mstar-msc313_miu.c
+++ drivers/memory/mstar-msc313_miu.c
@@ -42,6 +42,19 @@
  *                                  | 0x2 - 10 | 0x2 - 8 | 0x2 - 64bit | 0x2 - DDR2
  *        		            |          |         |             | 0x3 - DDR3
  * 
+ * 0x008 - config2
+ * |   4 - 0
+ * | rd timing
+ * | 0xD
+ *
+ * 0x00c - config3
+ *
+ * 0x010 - config4
+ *
+ *    15   |    14    | 13 - 8 | 7 - 4  | 3 - 0
+ * trp msb | trcd msb |  trs   |  trp   | trcd
+ *    0    |     0    |  0x1e  |  0x9   | 0x9
+ *
  * The vendor suspend code writes 0xFFFF to all of these
  * but the first where it writes 0xFFFE instead
  * Presumably this is to stop requests happening while it
@@ -64,17 +77,33 @@
  * 0x04c - group 5 request mask?
  */
 
-#define REG_CONFIG1		0x4
-#define REG_CONFIG1_TYPE	(BIT(1) | BIT(0))
-#define REG_CONFIG1_TYPE_SDR	0
-#define REG_CONFIG1_TYPE_DDR	BIT(0)
-#define REG_CONFIG1_TYPE_DDR2	BIT(1)
-#define REG_CONFIG1_TYPE_DDR3	(BIT(1) | BIT(0))
-#define REG_CONFIG1_BUSWIDTH	(BIT(3) | BIT(2))
-#define REG_CONFIG1_BANKS	(BIT(5) | BIT(4))
-#define REG_CONFIG1_BANKS_SHIFT	4
-#define REG_CONFIG1_COLS	(BIT(7) | BIT(6))
-#define REG_CONFIG1_COLS_SHIFT	6
+#define REG_CONFIG1			0x4
+#define REG_CONFIG1_TYPE		(BIT(1) | BIT(0))
+#define REG_CONFIG1_TYPE_SDR		0
+#define REG_CONFIG1_TYPE_DDR		BIT(0)
+#define REG_CONFIG1_TYPE_DDR2		BIT(1)
+#define REG_CONFIG1_TYPE_DDR3		(BIT(1) | BIT(0))
+#define REG_CONFIG1_BUSWIDTH		(BIT(3) | BIT(2))
+#define REG_CONFIG1_BUSWIDTH_SHIFT	2
+#define REG_CONFIG1_BANKS		(BIT(5) | BIT(4))
+#define REG_CONFIG1_BANKS_SHIFT		4
+#define REG_CONFIG1_COLS		(BIT(7) | BIT(6))
+#define REG_CONFIG1_COLS_SHIFT		6
+
+#define REG_CONFIG2			0x8
+#define REG_CONFIG2_RD_TIMING		GENMASK(4,0)
+
+
+#define REG_CONFIG3			0xc
+
+#define REG_CONFIG4			0x10
+#define REG_CONFIG4_TRCD		GENMASK(3,0)
+#define REG_CONFIG4_TRP			GENMASK(7,4)
+#define REG_CONFIG4_TRP_SHIFT		4
+#define REG_CONFIG4_TRAS		GENMASK(13,8)
+#define REG_CONFIG4_TRAS_SHIFT		8
+#define REG_CONFIG4_TRCD_MSB		BIT(14)
+#define REG_CONFIG4_TRP_MSB		BIT(15)
 
 struct msc313_miu {
 	struct device *dev;
@@ -105,15 +134,61 @@ static const struct regmap_config msc313_miu_digital_regmap_config = {
 		.reg_stride = 4,
 };
 
+static const char *types[] = {"SDR", "DDR", "DDR2", "DDR3"};
+
+static int msc313_miu_read_trcd(struct msc313_miu *miu){
+	unsigned config4;
+	int ret;
+	ret = regmap_read(miu->digital, REG_CONFIG4, &config4);
+	if(!ret){
+		ret = (config4 & REG_CONFIG4_TRCD) +
+				(config4 & REG_CONFIG4_TRCD_MSB ? 1 << 4 : 0);
+	}
+	return ret;
+}
+
+static void msc313_miu_write_trcd(struct msc313_miu *miu, unsigned val){
+	regmap_update_bits(miu->digital, REG_CONFIG4, REG_CONFIG4_TRCD, val);
+}
+
+static int msc313_miu_read_trp(struct msc313_miu *miu){
+	unsigned config4;
+	int ret;
+	ret = regmap_read(miu->digital, REG_CONFIG4, &config4);
+	if(!ret){
+		ret = ((config4 & REG_CONFIG4_TRP) >> REG_CONFIG4_TRP_SHIFT) +
+				(config4 & REG_CONFIG4_TRP_MSB ? 1 << 4 : 0);
+	}
+	return ret;
+}
+
+static void msc313_miu_write_trp(struct msc313_miu *miu, unsigned val){
+	regmap_update_bits(miu->digital, REG_CONFIG4, REG_CONFIG4_TRP,
+			val << REG_CONFIG4_TRP_SHIFT);
+}
+
+static int msc313_miu_read_tras(struct msc313_miu *miu){
+	unsigned config4;
+	int ret;
+	ret = regmap_read(miu->digital, REG_CONFIG4, &config4);
+	if(!ret){
+		ret = (config4 & REG_CONFIG4_TRAS) >> REG_CONFIG4_TRAS_SHIFT;
+	}
+	return ret;
+}
+
 static int msc313_miu_probe(struct platform_device *pdev)
 {
 	struct msc313_miu *miu;
 	struct resource *mem;
 	__iomem void *base;
 	unsigned int config1;
-	const char *types[] = {"SDR", "DDR", "DDR2", "DDR3"};
-	int banks, cols;
 	
+	int banks, cols, buswidth;
+	int trcd, trp, tras;
+
+	u32 dtval;
+
 	miu = devm_kzalloc(&pdev->dev, sizeof(*miu), GFP_KERNEL);
 	if (!miu)
 		return -ENOMEM;
@@ -164,8 +239,32 @@ static int msc313_miu_probe(struct platform_device *pdev)
 
 	banks = 2  << ((config1 & REG_CONFIG1_BANKS) >> REG_CONFIG1_BANKS_SHIFT);
 	cols = 8 + ((config1 & REG_CONFIG1_COLS) >> REG_CONFIG1_COLS_SHIFT);
-	dev_info(&pdev->dev, "Memory type is %s, %d banks and %d columns", types[config1 & REG_CONFIG1_TYPE],
-			banks, cols);
+	buswidth = (((config1 & REG_CONFIG1_BUSWIDTH) >> REG_CONFIG1_BUSWIDTH_SHIFT) + 1) * 16;
+
+
+	dev_info(&pdev->dev, "Memory type is %s, %d banks and %d columns, %d bit bus", types[config1 & REG_CONFIG1_TYPE],
+			banks, cols, buswidth);
+
+	trcd = msc313_miu_read_trcd(miu);
+	trp = msc313_miu_read_trp(miu);
+	tras = msc313_miu_read_tras(miu);
+
+	dev_info(miu->dev, "trcd: %d, trp: %d, tras: %d", trcd, trp, tras);
+
+	if(!of_property_read_u32(pdev->dev.of_node, "mstar,rd-timing", &dtval)) {
+		dev_info(&pdev->dev, "Setting read back data delay to %d", (int) dtval);
+		//regmap_update_bits(miu->digital, REG_CONFIG2, REG_CONFIG2_RD_TIMING, rd_timing);
+	}
+
+	if(!of_property_read_u32(pdev->dev.of_node, "mstar,trcd", &dtval)) {
+		dev_info(&pdev->dev, "setting trcd to %d", (int) dtval);
+		msc313_miu_write_trcd(miu, dtval);
+	}
+
+	if(!of_property_read_u32(pdev->dev.of_node, "mstar,trp", &dtval)) {
+		dev_info(&pdev->dev, "setting trp to %d", (int) dtval);
+		msc313_miu_write_trp(miu, dtval);
+	}
 
 	return 0;
 }
